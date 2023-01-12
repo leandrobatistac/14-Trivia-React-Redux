1ce2df53237af314dd849125888ac942a9bc0b41
GiovanaZ <giovanazupiroli@gmail.com>
2023-01-12 14:53:29 -0300

    Requisito 3 OK

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Configuration from './pages/Configuration';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <header className="App-header">
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route path="/configuration" component={ Configuration } />
         </Switch>
       </header>
     </div>
